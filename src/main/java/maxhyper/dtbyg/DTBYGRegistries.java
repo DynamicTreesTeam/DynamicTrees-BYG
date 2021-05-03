d64221e36dc2a7b361c5e09b32ac33a4c3afc53e
supermassimo <massimocantugarcia@gmail.com>
2021-05-03 20:09:01 -0300

    Added poplar and yellow poplar trees

--- src/main/java/maxhyper/dtbyg/DTBYGRegistries.java
+++ src/main/java/maxhyper/dtbyg/DTBYGRegistries.java
@@ -3,9 +3,13 @@ package maxhyper.dtbyg;
 import com.ferreusveritas.dynamictrees.api.cells.CellKit;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
 import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
-import com.ferreusveritas.dynamictrees.cells.CellKits;
+import com.ferreusveritas.dynamictrees.growthlogic.GrowthLogicKit;
+import com.ferreusveritas.dynamictrees.trees.Species;
 import maxhyper.dtbyg.cells.DTBYGCellKits;
+import maxhyper.dtbyg.growthlogic.DTBYGGrowthLogicKits;
+import maxhyper.dtbyg.trees.PoplarSpecies;
 import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,8 +23,17 @@ public class DTBYGRegistries {
     }
 
     @SubscribeEvent
-    public static void registerLeavesPropertiesTypes (final TypeRegistryEvent<LeavesProperties> event) {
+    public static void onGrowthLogicKitRegistry (final com.ferreusveritas.dynamictrees.api.registry.RegistryEvent<GrowthLogicKit> event) {
+        DTBYGGrowthLogicKits.register(event.getRegistry());
+    }
 
+    @SubscribeEvent
+    public static void registerSpeciesTypes (final TypeRegistryEvent<Species> event) {
+        event.registerType(new ResourceLocation(DynamicTreesBYG.MOD_ID, "poplar"), PoplarSpecies.TYPE);
+    }
+
+    @SubscribeEvent
+    public static void registerLeavesPropertiesTypes (final TypeRegistryEvent<LeavesProperties> event) {
     }
 
     @SubscribeEvent
---
+++ src/main/java/maxhyper/dtbyg/growthlogic/DTBYGGrowthLogicKits.java
@@ -0,0 +1,16 @@
+package maxhyper.dtbyg.growthlogic;
+
+import com.ferreusveritas.dynamictrees.api.registry.Registry;
+import com.ferreusveritas.dynamictrees.growthlogic.GrowthLogicKit;
+import maxhyper.dtbyg.DynamicTreesBYG;
+import net.minecraft.util.ResourceLocation;
+
+public class DTBYGGrowthLogicKits {
+
+    public static final GrowthLogicKit POPLAR = new PoplarLogic(new ResourceLocation(DynamicTreesBYG.MOD_ID, "poplar"));
+
+    public static void register(final Registry<GrowthLogicKit> registry) {
+        registry.registerAll(POPLAR);
+    }
+
+}
