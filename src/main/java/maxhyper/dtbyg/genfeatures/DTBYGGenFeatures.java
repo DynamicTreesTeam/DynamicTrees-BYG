220a58490edc60b0f93dc14236640dccee46c99f
supermassimo <massimocantugarcia@gmail.com>
2022-01-05 23:39:42 -0300

    added worldgen for tropical fungal forest/rainforest
    also fixed araucaria seed and saplings

--- src/main/java/maxhyper/dtbyg/genfeatures/DTBYGGenFeatures.java
+++ src/main/java/maxhyper/dtbyg/genfeatures/DTBYGGenFeatures.java
@@ -1,6 +1,7 @@
 package maxhyper.dtbyg.genfeatures;
 
 import com.ferreusveritas.dynamictrees.api.registry.IRegistry;
+import com.ferreusveritas.dynamictrees.systems.genfeatures.BiomePredicateGenFeature;
 import com.ferreusveritas.dynamictrees.systems.genfeatures.GenFeature;
 import com.ferreusveritas.dynamictrees.systems.genfeatures.HugeMushroomGenFeature;
 import maxhyper.dtbyg.DynamicTreesBYG;
@@ -13,8 +14,11 @@ public class DTBYGGenFeatures {
     public static final GenFeature MANGROVELINGS = new MangrovelingsGenFeature(new ResourceLocation(DynamicTreesBYG.MOD_ID, "mangrovelings"));
     public static final GenFeature BRANCH_SPROUTS = new BranchSproutsGenFeature(new ResourceLocation(DynamicTreesBYG.MOD_ID, "branch_sprouts"));
     public static final GenFeature HUGE_MUSHROOMS = new BYGHugeMushroomGenFeature(new ResourceLocation(DynamicTreesBYG.MOD_ID, "huge_mushrooms"));
+    public static final GenFeature BIOME_PREDICATE_2 = new BiomePredicateGenFeature(new ResourceLocation(DynamicTreesBYG.MOD_ID, "biome_predicate"));
+
     public static void register(final IRegistry<GenFeature> registry) {
-        registry.registerAll(BIG_BOTTOM_FLARE, EXTRA_BOTTOM_FLARE, MANGROVELINGS, BRANCH_SPROUTS, HUGE_MUSHROOMS);
+        registry.registerAll(BIG_BOTTOM_FLARE, EXTRA_BOTTOM_FLARE, MANGROVELINGS,
+                BRANCH_SPROUTS, HUGE_MUSHROOMS, BIOME_PREDICATE_2);
     }
 
 }
